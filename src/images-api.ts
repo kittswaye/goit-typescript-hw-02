83f6ac3b14bf451a7ea3d3af719e5856a2f5c727
kittswaye <153619900+kittswaye@users.noreply.github.com>
2024-09-02 21:39:55 +0300

    fix

--- src/images-api.ts
+++ src/images-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.unsplash.com/search';
 const key: string = 'BBfUDZ9_xs1Fql0aSk2TLayxKF_kokkB-DtnsDQxJ-E';
 
-export const getImagesByTopic = async (topic: string, page: number): Promise<any> => {
+export const getImagesByTopic = async (topic: string, page: number) => {
   const response = await axios.get(`/photos?query=${topic}&page=${page}&per_page=15&orientation=landscape&client_id=${key}`);
   return response;
 };
